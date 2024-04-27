b4d3d396eaaad388cf1154f08493024997892b5e
yudhosaktirama <yudhosakti89@gmail.com>
2024-04-27 23:27:27 +0700

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: ListView(children: [
+        
+      ]),
+    );
+  }
+}
